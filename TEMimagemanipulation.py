bda7095d59d463277064acef4da7b299358cb055
magnunor <magnus.nord@ntnu.no>
2012-10-17 16:33:14 -0500

    Add getPoints. Which returns the points of mouseclicks.

--- TEMimagemanipulation.py
+++ TEMimagemanipulation.py
@@ -1,6 +1,32 @@
 from matplotlib.widgets import  RectangleSelector
 import matplotlib.pyplot as plt
 import numpy as np
+class PointsBuilder:
+  def __init__(self, data):
+    fig = plt.figure()
+    self.ax = fig.add_subplot(111)
+    self.ax.imshow(data)
+    self.xs = []
+    self.ys = []
+    self.cid = self.ax.figure.canvas.mpl_connect('button_press_event', self)
+    plt.show()
+    plt.close()
+
+  def __call__(self, event):
+    if event.inaxes!=self.ax.axes: return
+    self.xs.append(event.xdata)
+    self.ys.append(event.ydata)
+    xlim = self.ax.get_xlim()
+    ylim = self.ax.get_ylim()
+    for xpoint,ypoint in zip(self.xs,self.ys):
+      self.ax.plot(
+          [xpoint],
+          [ypoint],
+          marker='o',
+          mfc='None')
+    self.ax.set_xlim(xlim)
+    self.ax.set_ylim(ylim)
+    self.ax.figure.canvas.draw()
 
 class RectangleBuilder:
   def __init__(self, data):
@@ -17,28 +43,37 @@ class RectangleBuilder:
     self.x2 = None
     self.y1 = None
     self.y2 = None
-    self.template = None
+    self.subimage = None
 
     self.templateFig = plt.figure()
     self.templateFig.canvas.set_window_title('Subimage') 
     self.templatePlot = self.templateFig.add_subplot(111)
-    plt.tight_layout()
     plt.show()
 
     plt.close()
 
   def onselect(self, eclick, erelease):
+    #Matplotlib and numpy defines x and y orthogonal
+    #to eachother. Prompting this switch.
     self.y1, self.y2 = int(eclick.xdata), int(erelease.xdata)
     self.x1, self.x2 = int(eclick.ydata), int(erelease.ydata)
-    self.template = self.data[
+    self.subimage = self.data[
       self.x1:self.x2,
       self.y1:self.y2]
-    self.templatePlot.imshow(self.template) 
+    self.templatePlot.imshow(self.subimage) 
     self.templateFig.canvas.draw()
 
 def getSubImage(data):
-  getTemplate = RectangleBuilder(data)
-  template = getTemplate.template
-  pos1 = getTemplate.x1, getTemplate.y1
-  pos2 = getTemplate.x2, getTemplate.y2
-  return(template, pos1, pos2)
+  tempSubImage = RectangleBuilder(data)
+  subImage = tempSubImage.subimage
+  pos1 = tempSubImage.x1, tempSubImage.y1
+  pos2 = tempSubImage.x2, tempSubImage.y2
+  return(subImage, pos1, pos2)
+
+def getPoints(data):
+  tempPoints = PointsBuilder(data)
+  #Matplotlib and numpy defines x and y orthogonal
+  #to eachother. Prompting this switch.
+  xpoints = tempPoints.ys
+  ypoints = tempPoints.xs
+  return(xpoints, ypoints)
--- example.py
+++ example.py
@@ -12,3 +12,11 @@ subImageArray = TEMimagemanipulation.getSubImage(data)
 subImage = subImageArray[0]
 position1 = subImageArray[1]
 position2 = subImageArray[2]
+
+pointsArray = TEMimagemanipulation.getPoints(data)
+xpoints = pointsArray[0]
+ypoints = pointsArray[0]
+
+print(subImage)
+print(position1, position2)
+print(xpoints, ypoints)
